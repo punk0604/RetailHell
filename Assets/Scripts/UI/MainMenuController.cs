976e820f09e129b2719c805dd715f47d7c129e2d
Ashlyn Horton <punkhorton0604@gmail.com>
2025-05-05 03:29:16 -0400

    start game and exit game button functionality

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -8,83 +8,78 @@ using Unity.VisualScripting;
 
 public class MainMenuController : MonoBehaviour
 {
-    public Button[] menuButtons; // Array of menu buttons
-    private int selectedIndex = 0; // Keeps track of which button is selected
+    public Button[] menuButtons;
+    private int selectedIndex = 0;
     public GameObject options;
     OptionsUI optionsUI;
 
     void Start()
     {
-        HighlightButton(); // Ensure the first button is highlighted
+        HighlightButton();
         optionsUI = options.GetComponent<OptionsUI>();
     }
 
     void Update()
     {
-        // Navigate Down
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             selectedIndex = (selectedIndex + 1) % menuButtons.Length;
             HighlightButton();
         }
 
-        // Navigate Up
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             selectedIndex = (selectedIndex - 1 + menuButtons.Length) % menuButtons.Length;
             HighlightButton();
         }
 
-        // Select Option
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            menuButtons[selectedIndex].onClick.Invoke(); // Simulate a button click
+            menuButtons[selectedIndex].onClick.Invoke();
         }
     }
 
-     void HighlightButton()
+    void HighlightButton()
     {
-        // Reset all button texts to default
         foreach (Button button in menuButtons)
         {
             TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.color = Color.white; // Default color
-            buttonText.fontStyle = FontStyles.Normal; // Remove underline/bold
+            buttonText.color = Color.white;
+            buttonText.fontStyle = FontStyles.Normal;
         }
 
-        // Highlight selected button's text
         TextMeshProUGUI selectedText = menuButtons[selectedIndex].GetComponentInChildren<TextMeshProUGUI>();
-        selectedText.color = Color.yellow; // Highlight color
-        selectedText.fontStyle = FontStyles.Bold | FontStyles.Underline; // Bold + Underline for emphasis
+        selectedText.color = Color.yellow;
+        selectedText.fontStyle = FontStyles.Bold | FontStyles.Underline;
     }
 
     public void StartGame()
     {
-        PlayUiSound(); //plays ui button sound
-        Debug.Log("Start Game"); // Placeholder for starting the game
-        StartmenuMusicManager.instance.GetComponent<AudioSource>().Stop(); //VERY important to stop the menu music when the game starts
+        PlayUiSound();
+        Debug.Log("Start Game");
+
+        StartmenuMusicManager.instance.GetComponent<AudioSource>().Stop();
+
+        SceneManager.LoadScene("retailStore"); // Load main scene
     }
 
     public void ShowInstructions()
     {
-        GameObject.Find("UI button sound").GetComponent<AudioSource>().Play(); //plays ui button sound
+        PlayUiSound();
         Debug.Log("Instructions Button Clicked - Show Instructions UI or Scene");
-    }
-
-    public void LoadOptions()
-    {
-        Time.timeScale = 0;
-        Debug.Log("time paused");
-        PlayUiSound(); //plays ui button sound
-        optionsUI.ShowOptions();
-        Debug.Log("Load Options Menu"); // Placeholder for loading options
     }
 
     public void QuitGame()
     {
-        StartmenuMusicManager.instance.GetComponent<AudioSource>().Stop(); //stops music just in case quitting the app breaks the music somehow
-        Application.Quit(); // Quit the application
-        Debug.Log("Game Quit"); // Log for debugging purposes
+        PlayUiSound();
+        StartmenuMusicManager.instance.GetComponent<AudioSource>().Stop();
+        Debug.Log("Game Quit");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop play mode in the editor
+#else
+        Application.Quit(); // Quit actual build
+#endif
     }
 
     public void PlayUiSound()
@@ -92,3 +87,4 @@ public class MainMenuController : MonoBehaviour
         GameObject.Find("UI button sound").GetComponent<AudioSource>().Play();
     }
 }
+
